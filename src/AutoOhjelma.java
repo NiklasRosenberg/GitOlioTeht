60a6c229bf07865fe4a180fa3aaef8573bdd5ee7
NiklasRosenberg <91270720+NiklasRosenberg@users.noreply.github.com>
2021-12-01 18:51:42 +0200

    vaiheet 1-3

--- src/AutoOhjelma.java
+++ src/AutoOhjelma.java
@@ -2,8 +2,43 @@
 public class AutoOhjelma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
+}//mainin loppu
+
+class Auto{
+
+	 public String merkki;
+	 public String malli;
+	 public int bensanMaara;
+	 
+	 
+public void jarruta() {
+		 
+		 System.out.println("Auto jarruttaa");
+	 }
+	 
+	 public void kiihdyta() {
+		 
+		 if (bensanMaara > 0) {
+		 System.out.println("Auto kiihtyy");
+		 
+		 bensanMaara = bensanMaara -1;
+		 }
+		 
+		 else {
+			 System.out.println("Tankkaa auto");
+		 }
+		 
+	 }
+	 
+	 public void naytaTiedot() {
+		 
+		 System.out.println("Auton merkki: " + merkki);
+		 System.out.println("Auton malli: " + malli);
+		 System.out.println("Polttoainetta tankissa: " + bensanMaara + " litraa");
+	 }
+	 
 }

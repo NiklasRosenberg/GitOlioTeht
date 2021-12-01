3bcc166acb7c99d1dcab920c4acb17d21987d992
NiklasRosenberg <91270720+NiklasRosenberg@users.noreply.github.com>
2021-12-01 18:54:17 +0200

    auto1 ja auto2 + konstruktori

--- src/AutoOhjelma.java
+++ src/AutoOhjelma.java
@@ -3,19 +3,44 @@ public class AutoOhjelma {
 
 	public static void main(String[] args) {
 		
+		Auto auto1 = new Auto();
+		
+		auto1.naytaTiedot();
+		auto1.kiihdyta();
+		auto1.naytaTiedot();
+		auto1.tankkaa(5);
+		auto1.naytaTiedot();
+		
+		Auto auto2 = new Auto("Jaguar", "XE", 30);
+		
+		auto2.naytaTiedot();
 
 	}
 
 }//mainin loppu
 
-class Auto{
+ class Auto{
 
 	 public String merkki;
 	 public String malli;
 	 public int bensanMaara;
 	 
+	 public Auto() {
+		 
+		 merkki = "";
+		 malli = "";
+		 bensanMaara = 0;
+	 }
 	 
-public void jarruta() {
+	 public Auto(String brand, String model, int fuelValue) {
+		 
+		 merkki = brand;
+		 malli = model;
+		 bensanMaara = fuelValue;
+		 
+	 }
+	 
+	 public void jarruta() {
 		 
 		 System.out.println("Auto jarruttaa");
 	 }
